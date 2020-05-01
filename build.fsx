b75bd6fc123d9712e4c256827137318af02a0ede
Benjamin Gobeil <ben_gob@hotmail.com>
2020-05-01 18:41:15 -0400

    Continue the build script

--- build.fsx
+++ build.fsx
@@ -1,7 +1,12 @@
+#if !FAKE
+#r "netstandard"
+#endif
+
 #r "paket:
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Target //"
+
 #load ".fake/build.fsx/intellisense.fsx"
 open Fake.Core
 open Fake.DotNet
@@ -11,17 +16,63 @@ open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
 Target.initEnvironment ()
+let projectPath = "./src/Feliz.Isomorphic"
+let generatorPath = "./src/Generator"
 
+let platformTool tool winTool =
+  let tool = if Environment.isUnix then tool else winTool
+  tool
+  |> ProcessUtils.tryFindFileOnPath
+  |> function Some t -> t | _ -> failwithf "%s not found" tool
+  
+let runTool cmd args workingDir =
+    let arguments = args |> String.split ' ' |> Arguments.OfArgs
+    Command.RawCommand (cmd, arguments)
+    |> CreateProcess.fromCommand
+    |> CreateProcess.withWorkingDirectory workingDir
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+    
+let runDotNet cmd workingDir =
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+    
+let delete file =
+    if System.IO.File.Exists(file)
+    then System.IO.File.Delete file
+    else ()
+  
 Target.create "Clean" (fun _ ->
     !! "src/**/bin"
     ++ "src/**/obj"
     |> Shell.cleanDirs 
 )
 
-Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+Target.create "RunGenerator" (fun _ ->
+    runDotNet "run" generatorPath 
 )
+
+Target.create "BuildIsomorphic" (fun _ ->
+    runDotNet "build" projectPath
+)
+
+let publish projectPath = fun _ ->
+    runDotNet "restore --no-cache" projectPath
+    runDotNet "pack -c Release" projectPath
+    let nugetKey =
+        match Environment.environVarOrNone "FELIZ_ISOMORPHIC_NUGET_KEY" with
+        | Some nugetKey -> nugetKey
+        | None -> failwith "The Nuget API key must be set in a NUGET_KEY environmental variable"
+    let nupkg =
+        Directory.findFirstMatchingFile "*.nupkg" (projectPath </> "bin" </> "Release")
+        
+    let pushCmd = sprintf "nuget push %s -s nuget.org -k %s" nupkg nugetKey
+    runDotNet pushCmd projectPath
+    
+Target.create "Publish" (publish projectPath)
+    
 
 Target.create "All" ignore
 
